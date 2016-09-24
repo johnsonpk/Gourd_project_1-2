14639a3a7af4ff13ed9ba274500a213fbd258586
Pablo Johnson <paulj.96@gmail.com>
2016-09-24 14:35:11 -0500

    added comments to p1, took out some fluff

--- Gourd01.java
+++ Gourd01.java
@@ -12,17 +12,22 @@ public class Gourd01
 		String nextLine;
 		Scanner scanner = new Scanner(System.in);
 
+		// While we have a line to be read ...
 		while(scanner.hasNextLine())
 		{
+			// Grab the next line
 			nextLine = scanner.nextLine();
 			
+			//  parse the first character to an integer 
 			num = Integer.parseInt(nextLine.substring(0,1));
 
+			// For whatever integer we have, add one to that place in our leading_digit array
 			leading_digit[num] += 1;
 		} 
 
 		for(int i: leading_digit)
 		{
+			// calculate sum of leading_digits
 			sum += i;
 		}
 
@@ -32,30 +37,24 @@ public class Gourd01
 		
 		for(int i = 0; i < 10; i++)
 		{
+			// Number of spaces between first digit of our Count integer and the period of our floating point
 			num = 11;
 			nextLine = "";
+			// Reduce num by our length of our count integer and our digits in front of the decimal point of our floating point 
 			num -= (Integer.toString(leading_digit[i]).length() + Integer.toString(100 * leading_digit[i]/sum).length());
 			
 
 			for(int j = 0; j < num; j ++)
 			{
+				// make nextLine equal the num * " "
 				nextLine += " ";
 			}
-//			System.out.printf("Length %d:%d:%d\n",Integer.toString(leading_digit[i]).length(),Integer.toString(100 * leading_digit[i]/sum).length(),nextLine.length());
+			// Print output
 			System.out.printf("%d               %d%s%.2f%%\n",i, leading_digit[i],nextLine, (100 * leading_digit[i] / (double)sum));
-		//	nextLine = StringRepair(num, sum, leading_digit, "");
-/*
-			nextLine = "";
-			nextLine = nextLine.format("%d                    ,%d      %.2f%%", num, leading_digit[num], (leading_digit[num] / (double)sum));
-			while((nextLine.indexOf(",") != 14))
-			{
-				nextLine = nextLine.substring(0,nextLine.indexOf(" ")) + nextLine.substring(nextLine.indexOf(" ") + 1);
-			}
-			nextLine = nextLine.replace(',', ' ');
-*/
-			
 		
 		}
+		
+		// Do the same as above, but for our final sum line
 		num = 8;
 		nextLine = "";
 		num -= Integer.toString(sum).length();
